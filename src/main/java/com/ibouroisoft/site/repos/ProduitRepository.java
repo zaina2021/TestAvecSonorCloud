2d2d72bc77870be653af97c6ffed1a414e6d940d
zaina2021 <ibouroimbaezaina@gmail.com>
2023-03-09 15:01:39 +0100

    Création de la méthode findByNomProduit dans l’interface ProduitRepository

--- src/main/java/com/ibouroisoft/site/repos/ProduitRepository.java
+++ src/main/java/com/ibouroisoft/site/repos/ProduitRepository.java
@@ -1,5 +1,7 @@
 package com.ibouroisoft.site.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ibouroisoft.site.entities.Produit;
 
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+	List<Produit> findByNomProduit(String nom);
+	
 
 }
