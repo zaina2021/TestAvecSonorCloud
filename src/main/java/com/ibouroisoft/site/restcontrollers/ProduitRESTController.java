0811601e4668d85f482bdfa5e211b7d254d9a8a2
zaina2021 <ibouroimbaezaina@gmail.com>
2023-03-09 16:30:39 +0100

    Créer la classe ProduitRESTController dans le package com.ibouroisoft.site.restcontrollers

---
+++ src/main/java/com/ibouroisoft/site/restcontrollers/ProduitRESTController.java
@@ -0,0 +1,27 @@
+package com.ibouroisoft.site.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibouroisoft.site.entities.Produit;
+import com.ibouroisoft.site.services.ProduitService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProduitRESTController {
+	@Autowired
+	ProduitService produitService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Produit> getAllProduits(){
+		return produitService.getAllProduits();
+		
+	}
+
+}
