31e554b6238644c3c663ba5f05346ffe21e1f627
zaina2021 <ibouroimbaezaina@gmail.com>
2023-03-10 15:32:00 +0100

    Créer le Web service REST permettant de modifier un produit

--- src/main/java/com/ibouroisoft/site/restcontrollers/ProduitRESTController.java
+++ src/main/java/com/ibouroisoft/site/restcontrollers/ProduitRESTController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,24 @@ public class ProduitRESTController {
 	public Produit getProduitById(@PathVariable("id") Long id) {
 		return produitService.getProduit(id);
 	}
-
+	
+	/*
+	 * Créer le Web service REST permettant de créer un produit
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public Produit createProduit(@RequestBody Produit produit) {
+	return produitService.saveProduit(produit);
+	}
+	
+	/*
+	 * Créer le Web service REST permettant de modifier un produit
+	 */
+	@RequestMapping(method = RequestMethod.PUT)
+	public Produit updateProduit(@RequestBody Produit produit) {
+	return produitService.updateProduit(produit);
+	}
+	
+	
 }
 
 
